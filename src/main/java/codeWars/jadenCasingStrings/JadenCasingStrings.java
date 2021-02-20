707653fad7b567c6e6bd0a855e22bcedb0554828
garczynskiDominik <garczynski.dominik@gmail.com>
2021-02-20 21:17:52 +0100

    Jaden Casing Strings Kata

---
+++ src/main/java/codeWars/jadenCasingStrings/JadenCasingStrings.java
@@ -0,0 +1,38 @@
+package codeWars.jadenCasingStrings;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class JadenCasingStrings {
+    public static void main(String[] args) {
+        System.out.println(toJadenCase("How can mirrors be real if our eyes aren't real"));
+    }
+
+    public static String toJadenCase(String text) {
+        if (null == text || text.length() == 0) {
+            return null;
+        }
+
+        return Arrays.stream(text.split(" "))
+                .map(i -> i.substring(0, 1).toUpperCase() + i.substring(1))
+                .collect(Collectors.joining(" "));
+    }
+}
+
+/**
+ * Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013).
+ * Jaden is also known for some of his philosophy that he delivers via Twitter.
+ * When writing on Twitter, he is known for almost always capitalizing every word.
+ * For simplicity, you'll have to capitalize each word,
+ * check out how contractions are expected to be in the example below.
+ * <p>
+ * Your task is to convert strings to how they would be written by Jaden Smith.
+ * The strings are actual quotes from Jaden Smith,
+ * but they are not capitalized in the same way he originally typed them.
+ * <p>
+ * Example:
+ * <p>
+ * Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+ * Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+ * Note that the Java version expects a return value of null for an empty string or null.
+ */
