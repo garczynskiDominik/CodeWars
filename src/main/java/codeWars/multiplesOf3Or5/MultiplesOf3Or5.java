bb3b4506bd1de1abe0b2d3d39a8052ad0c477bff
garczynskiDominik <garczynski.dominik@gmail.com>
2021-02-22 11:24:44 +0100

    Multiples of 3 or 5 Kata update

--- src/main/java/codeWars/multiplesOf3Or5/MultiplesOf3Or5.java
+++ src/main/java/codeWars/multiplesOf3Or5/MultiplesOf3Or5.java
@@ -1,18 +1,16 @@
 package codeWars.multiplesOf3Or5;
 
+import java.util.stream.IntStream;
+
 public class MultiplesOf3Or5 {
     public static void main(String[] args) {
         System.out.println(solution(10));
     }
 
     public static int solution(int number) {
-        int sum = 0;
-        for (int i = 0; i < number; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                sum += i;
-            }
-        }
-        return sum;
+        return IntStream.range(0, number)
+                .filter(n -> (n % 3 == 0) || (n % 5 == 0))
+                .sum();
     }
 }
 
