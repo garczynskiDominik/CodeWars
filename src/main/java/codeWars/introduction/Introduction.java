00cd04147ca274c698e1a7beffd1d5b6b62740c1
garczynskiDominik <garczynski.dominik@gmail.com>
2021-02-23 08:37:11 +0100

    Introduction Kata

---
+++ src/main/java/codeWars/introduction/Introduction.java
@@ -0,0 +1,36 @@
+package codeWars.introduction;
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println(century(0));
+    }
+
+    public static int century(int number) {
+        if (number == 0) return 1;
+
+        if (number % 100 == 0) {
+            number = number / 100;
+        } else {
+            number = (number / 100) + 1;
+        }
+        return number;
+
+    }
+
+}
+
+/**
+ * Introduction
+ * The first century spans from the year 1 up to and including the year 100,
+ * The second - from the year 101 up to and including the year 200, etc.
+ * <p>
+ * Task :
+ * Given a year, return the century it is in.
+ * <p>
+ * Input , Output Examples ::
+ * centuryFromYear(1705)  returns (18)
+ * centuryFromYear(1900)  returns (19)
+ * centuryFromYear(1601)  returns (17)
+ * centuryFromYear(2000)  returns (20)
+ * Hope you enjoy it .. Awaiting for Best Practice Codes
+ */
