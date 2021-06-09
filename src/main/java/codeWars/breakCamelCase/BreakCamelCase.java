14d2d77e69bc89b5f8efaf0c230245a861e2e39c
garczynskiDominik <garczynski.dominik@gmail.com>
2021-06-09 09:53:57 +0200

    Break Camel Case upgrade

--- src/main/java/codeWars/breakCamelCase/BreakCamelCase.java
+++ src/main/java/codeWars/breakCamelCase/BreakCamelCase.java
@@ -6,16 +6,6 @@ public class BreakCamelCase {
     }
 
     public static String camelCase(String input) {
-        int character;
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < input.length(); i++) {
-            character = input.charAt(i);
-            if (character >=65 && character<=90) {
-                stringBuilder.append(" ");
-            }
-                stringBuilder.append( input.charAt(i));
-        }
-        return stringBuilder.toString();
+        return input.replaceAll("([A-Z])", " $1");
     }
 }
